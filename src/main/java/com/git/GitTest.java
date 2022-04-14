64242881fd3742c045b0d55151e1acab1ecec853
zzz <zzz@zzz.com>
2022-04-14 16:15:29 +0900

    third commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -6,6 +6,7 @@ package com.git;
  */
 public class GitTest {
     public static void main(String[] args) {
+        System.out.println("hello git!");
         System.out.println("hello git3!");
     }
 }
