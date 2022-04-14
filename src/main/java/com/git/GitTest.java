f36154b6522e757ad44f4807d5d7ae29c0a9fcaf
zzz <zzz@zzz.com>
2022-04-14 15:54:37 +0900

    first commit

---
+++ src/main/java/com/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.git;
+
+/**
+ * @author User
+ * @create 2022-04-14 15:47
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
